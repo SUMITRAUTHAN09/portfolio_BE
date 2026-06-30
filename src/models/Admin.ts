1a68ef3d4a080dc541efb98574287f8f76a495c3
Sumit_Rauthan <sumitrauthan413@gmail.com>
2026-06-30 23:15:41 +0530

    Add: Admin model interface and schema

---
+++ src/models/Admin.ts
@@ -0,0 +1,16 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IAdmin extends Document{
+  email:string,
+  password:string,
+}
+
+const AdminSchema =new Schema<IAdmin>(
+  {
+    email:{type:String, required:true, unique:true},
+    password:{type:String, required:true},
+  },
+  {timestamps:true},
+)
+
+export default mongoose.model<IAdmin>("Admin",AdminSchema);
